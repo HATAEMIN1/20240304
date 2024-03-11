bf970c8fad0399e9cd52d9e22fa01cf33b86cd23
HaTaeMin <gkxoals33@gmail.com>
2024-03-11 18:36:25 +0900

    2024-03-11

---
+++ 02javascript/20240311/car.js
@@ -0,0 +1,50 @@
+var car = [
+  {
+    name: "Sonata",
+    speed: 10,
+    color: "white",
+    door: 4,
+    start: function () {
+      return this.speed;
+    },
+  },
+  {
+    name: "Canival",
+    speed: 100,
+    color: "red",
+    door: 4,
+    start: function () {
+      return this.speed + 100;
+    },
+  },
+  {
+    name: "Canival1",
+    speed: 100,
+    color: "red",
+    door: 4,
+    start: function () {
+      return this.speed + 100;
+    },
+  },
+  {
+    name: "Canival2",
+    speed: 100,
+    color: "red",
+    door: 4,
+    start: function () {
+      return this.speed + 100;
+    },
+  },
+  {
+    name: "Canival3",
+    speed: 100,
+    color: "red",
+    door: 4,
+    start: function () {
+      return this.speed + 100;
+    },
+  },
+];
+
+var cycle = [1, 2, 3];
+export default car;
---
+++ 02javascript/20240311/test.js
@@ -0,0 +1,10 @@
+var car = {
+  name: "Sonata",
+  speed: 10,
+  color: "white",
+  door: 4,
+  start: function () {
+    return this.speed;
+  },
+};
+console.log(car.name);
