34134d912059c90b710e412e240b8fa67b79c510
HaTaeMin <gkxoals33@gmail.com>
2024-03-20 09:32:56 +0900

    20240320

--- 03node/01express/src/server.js
+++ 03node/01express/src/server.js
@@ -2,11 +2,10 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-
 const { User } = require("./model/User.js");
 
 const users = [];
-dotenv.config;
+dotenv.config();
 //process.env.MONGO_URL
 
 // let result = mongoose.connect(MONGO_URL);
@@ -51,6 +50,45 @@ const server = async function () {
         const user = new User(req.body);
         await user.save();
         res.send({ user });
+      } catch (error) {
+        return res.status(500).send({ error: error.message });
+      }
+    });
+
+    app.delete("/user/:userId", async function (req, res) {
+      try {
+        let { userId } = req.params;
+
+        if (!mongoose.isValidObjectId(userId)) {
+          return res.status(400).send({ error: "유저가 없네요!" });
+        }
+        const user = await User.findByIdAndDelete({ _id: userId });
+        res.send({ user });
+      } catch (error) {
+        return res.status(500).send({ error: error.message });
+      }
+    });
+
+    app.put("/user/:userId", async function (req, res) {
+      try {
+        let { userId } = req.params;
+        let { age } = req.body;
+        if (!mongoose.isValidObjectId(userId)) {
+          return res.status(400).send({ error: "유저가 없네요!" });
+        }
+        if (!age) {
+          return res.status(400).send({ error: "나이입력해주세요!" });
+        }
+        if (typeof age !== "number") {
+          return res.status(400).send({ error: "숫자입력해주세요!" });
+        }
+
+        const user = await User.findByIdAndUpdate(
+          userId, //id로찾음
+          { $set: { age } }, //바꿀 키값
+          { new: true } //새로고침 후 바로 반영 ,없으면 2번해줘야함
+        );
+        return res.send({ user });
       } catch (error) {
         return res.status(500).send({ error: error.message });
       }
--- 03node/02express/server.js
+++ 03node/02express/server.js
@@ -1,42 +1,88 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const { User } = require("./User.js");
-const MONGO_URL =
-  "mongodb+srv://gkxoals33:0lUDwiTsPmcWsDG5@mongodb0.3ej5iia.mongodb.net/book?retryWrites=true&w=majority&appName=mongodb0";
+const dotenv = require("dotenv");
+
+const { User } = require("./src/model/User.js");
+
 const users = [];
+dotenv.config();
 const server = async function () {
-  await mongoose.connect(MONGO_URL);
-  app.use(express.json());
-  app.get("/user", async function (req, res) {
-    try {
-      const users = await User.find({});
-      return res.send({ users });
-    } catch (error) {
-      return res.status(500).send({ error: error.message });
-    }
-    const users = await User.find({});
-    res.send({ users });
-  });
-
-  app.post("/user", async function (req, res) {
-    try {
-      let { username, name } = req.body;
-      if (!username) {
-        return res.status(400).send({ error: "이름없음" });
+  try {
+    //DB연결
+    await mongoose.connect(process.env.MONGO_URL);
+    //JSON으로 변경해주는 미들웨어
+    app.use(express.json());
+    //GET
+    app.get("/user", async function (req, res) {
+      try {
+        const users = await User.find({});
+        return res.send({ users });
+      } catch (error) {
+        return res.status(500).send({ error: error.message });
       }
-      if (!name || !name.first || !name.latst) {
-        return res.status(400).send({ error: "성/이름이 없네요" });
+    });
+    //POST
+    app.post("/user", async function (req, res) {
+      try {
+        let { username, name } = req.body;
+        if (!username) {
+          return res.status(400).send({ error: "이름없음" });
+        }
+        if (!name || !name.first || !name.last) {
+          return res.status(400).send({ error: "성/이름이 없네요" });
+        }
+
+        console.log("자료" + req.body.username);
+        const user = new User(req.body);
+        await user.save();
+        res.send({ user });
+      } catch (error) {}
+    });
+    //DELETE
+    app.delete("/user/:userId", async function (req, res) {
+      try {
+        let { userId } = req.params;
+        if (!mongoose.isValidObjectId(userId)) {
+          return res.status(400).send({ error: "유저가 없네요!" });
+        }
+        const user = await User.findByIdAndDelete({ _id: userId });
+        res.send({ user });
+      } catch (e) {
+        return res.status(500).send({ error: error.message });
       }
+    });
+    //UPDATE
+    app.put("/user/:userId", async function (req, res) {
+      try {
+        let { userId } = req.params;
+        let { username, name, age, email } = req.body;
+        if (!mongoose.isValidObjectId(userId)) {
+          return res.status(400).send({ error: "유저가 없네요!" });
+        }
+        if (typeof age !== "number") {
+          return res.status(400).send({ error: "숫자만 입력해주세요" });
+        }
+        if (!username || !name || !age || email) {
+          return res
+            .status(400)
+            .send({ error: "유저이름,이름,나이,이메일을 입력하세요" });
+        }
 
-      console.log("자료" + req.body.username);
-      const user = new User(req.body);
-      await user.save();
-      res.send({ user });
-    } catch (error) {}
-  });
-
-  app.listen(3000);
+        const user = await User.findByIdAndUpdate(
+          userId,
+          { $set: { username, name, age, email } },
+          { new: true }
+        );
+        res.send({ user });
+      } catch (error) {
+        return res.status(500).send({ error: error.message });
+      }
+    });
+    app.listen(3000);
+  } catch (e) {
+    console.log("잘못 연결됨");
+  }
 };
 
 server();
